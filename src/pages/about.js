b6b1ec321bbbe5f9bc4918b71f9cdf3c71195a8d
Luminc <jeroen.kortekaas0508@gmail.com>
2022-09-06 14:42:28 +0200

    Added layout component and about

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/layout'
+
+const AboutPage = () => {
+    return (
+        <Layout pageTitle="About">
+            <p>Jeroen Kortekaas, (1991, NL) is an interdisciplinary artist working with a practice that shifts between psychogeography, research, drawing, sculpture and assemblage. His work utilises motifs of the network — such as infrastructure, signals and animation — as metaphors for distributed agency. In his work he wants to create spaces dynamic relation. Kortekaas sees a work of art as authored by not just the artist, but out of relation between the artwork and artist, that in-between space which allows the emergence of new subjectivities. The goal of this practice to allow the emergence of new ontological perspectives, generating novel pathways of constructing the self.</p>
+        </Layout>
+    )
+}
+export const Head = () => <title>Jeroen Kortekaas</title>
+export default AboutPage
