2d230f00e0c2247a0650efbe33bbbe59e49e5ab5
Luminc <jeroen.kortekaas0508@gmail.com>
2022-09-06 16:48:17 +0200

    Added dynamic routing for blog pages

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
-import { graphql} from 'gatsby'
-import Layout from '../components/layout'
-import Seo from '../components/seo'
+import { graphql, Link} from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 const BlogPage = ({data}) => {
   return (
     <Layout pageTitle="My Blog Posts">
@@ -9,7 +9,7 @@ const BlogPage = ({data}) => {
         {
             data.allMdx.nodes.map(node => (
             <article key={node.id}>
-                <h2>{node.frontmatter.title}</h2>
+                <h2><Link to={`/blog/${node.frontmatter.slug}`}>{node.frontmatter.title}</Link></h2>
                 <p>{node.frontmatter.date}</p>
                 <p>{node.excerpt}</p>
             </article>)
@@ -26,6 +26,7 @@ query {
       frontmatter {
         title
         date(formatString: "MMMM DD, YYYY")
+        slug
       }
       id
       excerpt
