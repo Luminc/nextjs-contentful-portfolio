621ed2ac54d7e47ef6ba6f5496b0cf36c16cad1f
Luminc <jeroen.kortekaas0508@gmail.com>
2025-06-14 15:22:09 +0200

    Add richtext to auto-generated pages

--- src/pages/{contentfulPage.slug}.js
+++ src/pages/{contentfulPage.slug}.js
@@ -3,7 +3,7 @@ import Layout from "../components/layout";
 import Seo from "../components/seo";
 import { graphql } from "gatsby";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
-// import ContentfulRichTech from "../components/contentful-rich-text";
+import ContentfulRichTech from "../components/contentful-rich-text";
 import Container from "react-bootstrap/Container";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
@@ -11,21 +11,19 @@ import { EmailForm } from "../components/emailform";
 
 const Page = ({ data }) => {
   const image = getImage(data.contentfulPage.image.gatsbyImageData);
+
   return (
     <Layout pageTitle={data.contentfulPage.title}>
       <Container size="xl">
         <Row>
           <Col md>
-            {" "}
             <GatsbyImage
               image={image}
               alt={data.contentfulPage.image.description}
             />
           </Col>
           <Col>
-            <p className="leading-loose">
-              {data.contentfulPage.description.internal.content}
-            </p>
+            <ContentfulRichTech richText={data.contentfulPage.richdescription} />
           </Col>
         </Row>
         <EmailForm />
@@ -42,10 +40,8 @@ export const data = graphql`
         gatsbyImageData
         description
       }
-      description {
-        internal {
-          content
-        }
+      richdescription {
+        raw
       }
     }
   }
